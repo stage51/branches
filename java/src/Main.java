87ec7974fb2eb7ada3702616e81606219aed1450
stage51 <sadzillaxxx@gmail.com>
2023-10-19 19:27:19 +0300

    Add loop

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке dev");
+        for (int i = 0; i < 5; i++){
+            System.out.print("Это добавлено в ветке dev");
+        }
     }
 }
