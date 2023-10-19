3f211e9ace78052c714c57b642edd1ba209300e4
stage51 <sadzillaxxx@gmail.com>
2023-10-19 19:28:20 +0300

    Add if condition

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++){
             System.out.print("Это добавлено в ветке dev");
+            if (i == 3) break;
         }
     }
 }
