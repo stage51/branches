49298ab307d6d87848fd3b9b81d185fd7ea4de94
stage51 <sadzillaxxx@gmail.com>
2023-10-19 19:07:41 +0300

    Add output

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Это добавлено в ветке main?");
-        calc(Integer.parseInt(scanner.nextLine()), Integer.parseInt(scanner.nextLine()));
+        System.out.println(calc(Integer.parseInt(scanner.nextLine()), Integer.parseInt(scanner.nextLine())));
     }
     public static int calc(int first, int second){
         return first+second;
