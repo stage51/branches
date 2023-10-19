0a22541b117e98d70b4e0565c18cb38bf211ad6c
stage51 <sadzillaxxx@gmail.com>
2023-10-19 19:05:52 +0300

    Create calc(int, int)

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Это добавлено в ветке main?");
-        scanner.nextLine();
+        calc(Integer.parseInt(scanner.nextLine()), Integer.parseInt(scanner.nextLine()))
+    }
+    public static int calc(int first, int second){
+        return first+second;
     }
 }
